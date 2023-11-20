0d6d889d9eca26a8995b243b829fd0ec93846847
Marat-Shainurov <m_shainurov@mail.ru>
2023-11-20 16:43:13 +0400

    updates db.py and docker-compose.yaml, adds env_example,

--- config/db.py
+++ config/db.py
@@ -5,13 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-client = MongoClient(os.getenv('CLIENT_ADDRESS_AND_PORT'), connect=False)
+client = MongoClient(os.getenv('DATABASE_CLIENT_ADDRESS_AND_PORT'), connect=False)
+collection_name = os.getenv('COLLECTION_NAME')
+db_name = os.getenv('DATABASE_NAME')
 
-db = client.get_database("db_determine_forms")
-forms_collection = db.get_collection("forms_collection")
+db = client.get_database(db_name)
+forms_collection = db.get_collection(collection_name)
 forms_collection.create_index('name', unique=True)
 
-if "forms_collection" not in db.list_collection_names():
-    db.create_collection("forms_collection")
+if collection_name not in db.list_collection_names():
+    db.create_collection(collection_name)
 
 client.server_info()
