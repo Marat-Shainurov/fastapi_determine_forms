7ed90d47933e2b2cdb5edf0dfeb2e6fac461ce72
Marat-Shainurov <m_shainurov@mail.ru>
2023-11-20 13:29:30 +0400

    updates the schema example for the FormStructureTemplate model

--- app/forms/models.py
+++ app/forms/models.py
@@ -1,28 +1,29 @@
 from typing import Dict
 
+from fastapi import HTTPException, status
 from pydantic import BaseModel, Field, model_validator
 
 
 class FormStructureTemplate(BaseModel):
-    name: str = Field(
-        ..., min_length=1, title='The Form name. Should be longer than 1 symbol', example='The Form name')
+    name: str = Field(..., min_length=1, title='Form template name.', examples=["Form template name"])
     fields: Dict[str, str] = Field(
-        ..., title='The Form fields. Available fields types - "email", "text", "phone", "date"',
-        example={
+        ..., title='Form fields. Available fields types - "email", "text", "phone", "date"',
+        examples=[{
             "field_name_1": "field_type",
             "field_name_2": "field_type",
             "field_name_n": "field_type"
-        }
+        }]
     )
 
     @model_validator(mode='before')
     @classmethod
     def check_field_types(cls, data):
         for field_name, field_type in data['fields'].items():
-            assert (
-                    field_type in ['email', 'phone', 'date', 'text']
-            ), (f"Invalid field type '{field_type}' for field '{field_name}'."
-                f"Supported types: 'email', 'phone', 'date', 'text'.")
+            if field_type not in ['email', 'phone', 'date', 'text']:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=f"Invalid field type - {field_type}. Supported types: email, phone, date, text."
+                )
         return data
 
 
