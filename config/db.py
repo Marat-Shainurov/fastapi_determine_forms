895745dbc403f299efcbb69fecd018ba02db6a01
Marat-Shainurov <m_shainurov@mail.ru>
2023-11-20 16:22:56 +0400

    updates the db configuration

--- config/db.py
+++ config/db.py
@@ -8,10 +8,10 @@ load_dotenv('.env')
 client = MongoClient(os.getenv('CLIENT_ADDRESS_AND_PORT'), connect=False)
 
 db = client.get_database("db_determine_forms")
-forms_collection = db.get_collection("determine_forms")
+forms_collection = db.get_collection("forms_collection")
 forms_collection.create_index('name', unique=True)
 
-if "determine_forms" not in db.list_collection_names():
-    db.create_collection("determine_forms")
+if "forms_collection" not in db.list_collection_names():
+    db.create_collection("forms_collection")
 
 client.server_info()
